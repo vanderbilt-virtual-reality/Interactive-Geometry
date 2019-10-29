c12b629ad9f3fc970eb279e15f1ade3fe21912bb
Kelson Reiss <kelson.reiss@gmail.com>
2019-10-29 18:51:01 -0500

    Modified Scripts to allow extending behavior
    * Untested on OVR headset *
    Customized OVRGrabber to make movement update method override-able
    ShapeScaler extends OVR Grabber. Should be attached as component to GameObject

---
+++ ShapeScaler.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeScaler : OVRGrabber
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        base.OnUpdatedAnchors();
+    }
+
+    // Hands follow the touch anchors by calling MovePosition each frame to reach the anchor.
+    // This is done instead of parenting to achieve workable physics. If you don't require physics on
+    // your hands or held objects, you may wish to switch to parenting.
+    protected override void OnUpdatedAnchors()
+    {
+        Vector3 handPos = OVRInput.GetLocalControllerPosition(m_controller);
+        Quaternion handRot = OVRInput.GetLocalControllerRotation(m_controller);
+        Vector3 destPos = m_parentTransform.TransformPoint(m_anchorOffsetPosition + handPos);
+        Quaternion destRot = m_parentTransform.rotation * handRot * m_anchorOffsetRotation;
+        GetComponent<Rigidbody>().MovePosition(destPos);
+        GetComponent<Rigidbody>().MoveRotation(destRot);
+
+        // DO THE MATH HERE
+        // Calculate distance between where hand originally was and where it is now
+        // That geometric distance should be the increased Y value in Scale of the object
+        // (TODO): How to measure distance in 3D plane? Maybe only consider 2D change 
+        float distance = destPos.magnitude - handPos.magnitude;
+
+        if (!m_parentHeldObject)
+        {
+            // Original Call
+            // MoveGrabbedObject(destPos, destRot);
+            AdjustObjectScale(distance);
+        }
+        m_lastPos = transform.position;
+        m_lastRot = transform.rotation;
+
+        float prevFlex = m_prevFlex;
+        // Update values from inputs
+        // CHANGING FROM PrimaryHandTrigger to PrimaryIndexTrigger to distinguish between grabbing and extending
+        m_prevFlex = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, m_controller);
+
+        CheckForGrabOrRelease(prevFlex);
+    }
+
+
+    protected void AdjustObjectScale(float distanceChange)
+    {
+        if (m_grabbedObj == null)
+        {
+            return;
+        }
+
+        GameObject gameObject = m_grabbedObj.gameObject;
+
+        // Found @https://www.youtube.com/watch?v=e7I315b74HY
+
+        // Could also probably use m_parentTransform.localScale because the parent of the script shoud be the GameObject
+        Vector3 scale = gameObject.transform.localScale;
+        scale.y += distanceChange;
+        gameObject.transform.localScale = scale;
+
+    }
+}
