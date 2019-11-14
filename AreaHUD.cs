e6fd18b1f174b28499ab8ec227c2dcf8612dd8ea
michaelzwu <35272608+michaelzwu@users.noreply.github.com>
2019-11-13 20:57:44 -0600

    Volume and Surface Area Heads Up Display Scripts

---
+++ AreaHUD.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AreaHUD : MonoBehaviour
+{
+    public float area;
+    public Text areaDisplay;
+
+    public float MeshArea(Vector3[] vertices, int[] triangles)
+    {
+        float result = 0;
+        for (int p = 0; p < triangles.Length; p += 3)
+        {
+            result += Vector3.Cross(vertices[triangles[p + 1]] - vertices[triangles[p]],
+                vertices[triangles[p + 2]] - vertices[triangles[p]]).magnitude;
+        }
+       result *= 0.5f;
+       return result;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3[] vertices = this.GetComponent<MeshFilter>().mesh.vertices;
+        int[] triangles = this.GetComponent<MeshFilter>().mesh.triangles;
+        area = MeshArea(vertices, triangles);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3[] vertices = this.GetComponent<MeshFilter>().mesh.vertices;
+        int[] triangles = this.GetComponent<MeshFilter>().mesh.triangles;
+        area = MeshArea(vertices, triangles);
+        area *= this.transform.localScale.x * this.transform.localScale.y * this.transform.localScale.z;
+        float temp = area;
+        temp.ToString("0.00");
+    }
+}
+
+// Source: // https://forum.unity.com/threads/figuring-out-the-volume-and-surface-area.117388/
---
+++ VolumeHUD.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeHUD : MonoBehaviour
+{
+    public float volume;
+    public Text volumeDisplay;
+
+    public float SignedVolumeOfTriangle(Vector3 p1, Vector3 p2, Vector3 p3)
+    {
+        float v321 = p3.x * p2.y * p1.z;
+        float v231 = p2.x * p3.y * p1.z;
+        float v312 = p3.x * p1.y * p2.z;
+        float v132 = p1.x * p3.y * p2.z;
+        float v213 = p2.x * p1.y * p3.z;
+        float v123 = p1.x * p2.y * p3.z;
+        return (1.0f / 6.0f) * (-v321 + v231 + v312 - v132 - v213 + v123);
+    }
+
+    public float VolumeOfMesh(Mesh mesh)
+    {
+        float volume = 0;
+        Vector3[] vertices = mesh.vertices;
+        int[] triangles = mesh.triangles;
+        for (int i = 0; i < mesh.triangles.Length; i += 3)
+        {
+            Vector3 p1 = vertices[triangles[i + 0]];
+            Vector3 p2 = vertices[triangles[i + 1]];
+            Vector3 p3 = vertices[triangles[i + 2]];
+            volume += SignedVolumeOfTriangle(p1, p2, p3);
+        }
+        return Mathf.Abs(volume);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
+        volume = VolumeOfMesh(mesh);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
+        volume = VolumeOfMesh(mesh);
+        volume *= this.transform.localScale.x * this.transform.localScale.y * this.transform.localScale.z;
+        // volume = mesh.bounds.size.x * mesh.bounds.size.y * mesh.bounds.size.z;
+    }
+}
